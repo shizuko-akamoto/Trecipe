e42194c2524fc3064eb9e40a16122d0c3fb85cdd
shizuko <aka-shizuko@hotmail.com>
2020-05-30 16:20:21 -0700

    Change Button diable and enable to single toggle method. Add dependencies to lodash (js library for various utility functions), and enzyme (for unit testing components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './fontawesome'
 import './stylesheets/App.scss';
-import './stylesheets/buttons.scss';
-import {Button} from "./components/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <h1>Trecipe</h1>
-          <Button text="Create New" icon="plus-circle"/>
       </header>
     </div>
   );
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,32 @@
 import React, {MouseEvent, Component} from "react";
-import '../stylesheets/buttons.scss'
+import "../stylesheets/buttons.scss"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {isUndefined} from "lodash";
 
+/**
+ * Button props
+ * text: text to display on button
+ * icon: font awesome icon name [optional]
+ * onClick: onClick handler function
+ */
 export interface ButtonProps {
     text: string;
     icon: IconProp;
     onClick(e: MouseEvent<HTMLElement>): void;
 }
 
+/**
+ * Button state
+ * disabled: true if button is diabled, false otherwise.
+ */
 export interface ButtonState {
     disabled: boolean;
 }
 
+/**
+ * A Button component configured with ButtonProps
+ */
 export class Button extends Component<ButtonProps, ButtonState> {
     public static defaultProps: Partial<ButtonProps> = {
         text: 'Button item',
@@ -26,18 +40,19 @@ export class Button extends Component<ButtonProps, ButtonState> {
             <div>
                 <button className="button" onClick={this.props.onClick} disabled={this.state.disabled}>
                     {this.props.text}
-                    {(this.props.icon !== undefined) &&
+                    {isUndefined(this.props.icon) &&
                     <span className="button-icon"><FontAwesomeIcon icon={this.props.icon} fixedWidth/></span>}
                 </button>
             </div>
         );
     }
 
-    public disable() {
-        this.setState({disabled: true})
-    }
-
-    public enable() {
-        this.setState({disabled: false})
+    /**
+     * Toggle on and off button
+     */
+    public toggle() {
+        this.setState((state: ButtonState) => ({
+            disabled: !state.disabled
+        }))
     }
 }
