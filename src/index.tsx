e1c54b03d264d1858b9f5b36b1b9cf12329554a3
UBC Student <nasu100@students.cs.ubc.ca>
2020-06-02 22:43:33 -0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { AddPopup } from './components/AddPopup';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddPopup />
   </React.StrictMode>,
   document.getElementById('root')
 );
