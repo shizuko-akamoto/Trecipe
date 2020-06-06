f06b238f8ba23cd875a7594fd482fdcc898f5425
shizuko <aka-shizuko@hotmail.com>
2020-06-06 09:42:06 -0700

    * Add trecipe cards to the state of MyTrecipe page (have to move it eventually to redux store probably) * Partially connect "Create New" button to MyTrecipe page. It can now display one more new card with the correct name input from AddPopup (The other TC properties aren't propagated yet, but this workflow is nice for demo purpose).

--- src/MyTercipes.tsx
+++ src/MyTercipes.tsx
@@ -4,13 +4,15 @@ import {Footer} from "./components/Footer";
 import './stylesheets/MyTrecipes.scss'
 import {FilterButton} from "./components/FilterButton";
 import {Button} from "./components/Button";
-import {TrecipeCard} from "./components/TrecipeCard";
+import {TCProps, TrecipeCard} from "./components/TrecipeCard";
 import {FilterSelector} from "./components/FilterSelector";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 import {AddPopup} from "./components/AddPopup";
 
 interface MyTrecipesState {
     addPopupOpen: boolean;
+    // TODO: Temporary for project_2, remove and load properly from redux store once redux is implemented
+    trecipeCards: Array<Partial<TCProps>>;
 }
 
 export class MyTrecipes extends React.Component<{}, MyTrecipesState> {
@@ -25,11 +27,15 @@ export class MyTrecipes extends React.Component<{}, MyTrecipesState> {
     };
 
     private createTrecipe = (e: React.MouseEvent<HTMLElement>, name: string, description: string) => {
+        this.setState((state: MyTrecipesState) => ({
+            trecipeCards: state.trecipeCards.concat({name: name})
+        }));
         this.closeAddPopup(e)
     };
 
     public readonly state: MyTrecipesState = {
-        addPopupOpen: false
+        addPopupOpen: false,
+        trecipeCards: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => TrecipeCard.defaultProps)
     };
 
     render() {
@@ -55,8 +61,8 @@ export class MyTrecipes extends React.Component<{}, MyTrecipesState> {
                         </div>
                     </div>
                     <div className="cards-wrapper">
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
-                            <div className="card-item" key={index}><TrecipeCard/></div>
+                        {this.state.trecipeCards.map((card: Partial<TCProps>, index) => (
+                            <div className="card-item" key={index}><TrecipeCard name={card.name}/></div>
                         ))}
                     </div>
                     {this.state.addPopupOpen && <div className="modal">
