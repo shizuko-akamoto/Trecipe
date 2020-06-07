ef816498a53224abd8b9cdd9492ed62042880d0d
shizuko <aka-shizuko@hotmail.com>
2020-06-07 01:22:21 -0700

    Add linear gradient overaly on trecipe card's image Modify scss a little with flex boxes to better lay out the title, privacy icon and edit button

--- src/components/TrecipeCard.tsx
+++ src/components/TrecipeCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Background from "../images/DefaultImage.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../stylesheets/trecipeCard.scss"
+import {CardMenu} from "./CardMenu";
 
 /**
  * Trecipe Props
@@ -36,27 +37,32 @@ export class TrecipeCard extends React.Component<TCProps>{
         completedDest: 3
     }
 
-    private percentage:number = this.props.completedDest/this.props.totalDest * 100;
+    private calcPercentage = (totalDest: number, completedDest: number) => {
+        return completedDest / totalDest * 100;
+    }
 
     render() {
         return (
-            <div className='TrecipeCard'>
-                <div className="Image" style={{ backgroundImage: this.props.imageSrc}}>
-                    <label> {this.props.name}</label>
-                    {this.props.isPrivate ? <FontAwesomeIcon icon="lock" className='icon' /> : null}
-                    <div className='icon ellipsis' ><FontAwesomeIcon icon={["fas", "ellipsis-h"]}/></div>
+            <div className='trecipeCard'>
+                <div className="tcHeaderContainer" style={{backgroundImage: this.props.imageSrc}}>
+                <div className="tcHeader">
+                    <label className="tcTitle">
+                        {this.props.name}
+                        <FontAwesomeIcon icon={(this.props.isPrivate) ? "lock" : "unlock"} className='tcPrivacy'/>
+                    </label>
+                    <div className="tcEdit"><CardMenu/></div>
                 </div>
-                <div className="Text">
-                    <div className='MetaData'>
-                        <div className='date'>{this.props.date}</div>
-                        <div className='author'>by: {this.props.author}</div>
+            </div>
+                <div className="tcBody">
+                    <div className='tcMetaData'>
+                        <div className='tcDate'>{this.props.date}</div>
+                        <div className='tcAuthor'>by: {this.props.author}</div>
                     </div>
-                    <div className="Description">
-                        <p>{this.props.description}</p>
-                    </div>
+                    <div className="tcDescription"><p>{this.props.description}</p></div>
                 </div>
-                <div className="ProgressBar">
-                    <div className="Filler" style={{ width: (this.percentage + "%")}}></div>
+                <div className="tcProgressBar">
+                    <div className="tcFiller" style={
+                        {width: this.calcPercentage(this.props.totalDest, this.props.completedDest) + "%"}}/>
                 </div>
             </div>
         );
