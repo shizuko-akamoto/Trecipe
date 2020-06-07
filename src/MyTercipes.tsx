83a0b91b87ff94fb21443fea52aa2091842c756a
shizuko <aka-shizuko@hotmail.com>
2020-06-07 01:39:06 -0700

    Connect AddPopup modal to MyTrecipes page. Make sure edit menu displays within the page (some position: absolute and position: relative in TC scss was messing with menu position calculation) Fix some paddings on My Trecipes header and TC items

--- src/MyTercipes.tsx
+++ src/MyTercipes.tsx
@@ -4,27 +4,46 @@ import {Footer} from "./components/Footer";
 import './stylesheets/MyTrecipes.scss'
 import {FilterButton} from "./components/FilterButton";
 import {Button} from "./components/Button";
-import {TrecipeCard} from "./components/TrecipeCard";
+import {TCProps, TrecipeCard} from "./components/TrecipeCard";
 import {FilterSelector} from "./components/FilterSelector";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {AddPopup} from "./components/AddPopup";
 
 interface MyTrecipesState {
     addPopupOpen: boolean;
+    // TODO: Temporary for project_2, remove and load properly from redux store once redux is implemented
+    trecipeCards: Array<Partial<TCProps>>;
 }
 
-export class MyTrecipes extends React.Component<{}, MyTrecipesState>{
+export class MyTrecipes extends React.Component<{}, MyTrecipesState> {
     private static contextFilters: string[] = ["All Trecipes", "Completed", "In Progress", "To Do"];
 
     private renderAddPopup = () => {
         this.setState({addPopupOpen: true})
     };
 
+    private closeAddPopup = (e: React.MouseEvent<HTMLElement>) => {
+        this.setState({addPopupOpen: false})
+    };
+
+    private createTrecipe = (e: React.MouseEvent<HTMLElement>, name: string, description: string) => {
+        this.setState((state: MyTrecipesState) => ({
+            trecipeCards: state.trecipeCards.concat({name: name})
+        }));
+        this.closeAddPopup(e)
+    };
+
+    public readonly state: MyTrecipesState = {
+        addPopupOpen: false,
+        trecipeCards: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => TrecipeCard.defaultProps)
+    };
+
     render() {
         return (<div>
                 <Header/>
-                <div className="body-wrapper">
-                    <body className="body">
-                    <h1 className="page-title">Trecipes</h1>
+                <div className="content-wrapper">
+                    <div className="content">
+                    <h1 className="page-title">My Trecipes</h1>
                     <div className="buttons-wrapper">
                         <ul className="context-filters">
                             {MyTrecipes.contextFilters.map((filter) =>
@@ -42,13 +61,17 @@ export class MyTrecipes extends React.Component<{}, MyTrecipesState>{
                         </div>
                     </div>
                     <div className="cards-wrapper">
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
-                            <div className="card-item"><TrecipeCard/></div>
+                        {this.state.trecipeCards.map((card: Partial<TCProps>, index) => (
+                            <div className="card-item" key={index}><TrecipeCard name={card.name}/></div>
                         ))}
                     </div>
-                    </body>
+                    {this.state.addPopupOpen && <div className="modal">
+                        <AddPopup onClose={this.closeAddPopup} onCreate={this.createTrecipe}/>
+                    </div>}
+                    {this.state.addPopupOpen && <div className="modal-overlay" onClick={this.closeAddPopup}/>}
                 </div>
                 <Footer/>
+            </div>
             </div>
         )
     }
