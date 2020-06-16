ae1b6106ca83d2dd32877263cfd45eaada58e0f3
shizuko <aka-shizuko@hotmail.com>
2020-06-16 00:47:58 -0700

    * Use react-beautiful-dnd to implement drag and drop reordering. * Create destination list view and finish basic set up of Trecipe page. *Implement edit toggling logic (both in Trecipe and DC component) * Implement Show More logic for controlling how many DC in list are visible (Default is 5 initially, incremented by 5 everytime Show More button is clicked).

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,12 @@ import React from "react";
 import "./components/fontawesome";
 import "./App.scss";
 import ModalContainer from "./components/Modal/ModalContainer";
-import {
-  DestinationCard,
-  DestinationCategory,
-} from "./pages/Trecipe/DestinationCard/DestinationCard";
-import SampleDestImage from "./pages/Trecipe/sample.jpg";
+import { Trecipe } from "./pages/Trecipe/Trecipe";
 
 function App() {
   return (
     <div className="App">
-      <DestinationCard
-        index={1}
-        destModel={{
-          id: 0,
-          name: "Destination Name",
-          category: DestinationCategory.Food,
-          address: "City, Country",
-          rating: 5,
-          description: "Some overview on this destination.",
-          imgSrc: SampleDestImage,
-        }}
-        onClickDelete={() => {}}
-      />
+      <Trecipe />
       <ModalContainer />
     </div>
   );
--- src/pages/Trecipe/DestinationCard/DestinationCard.tsx
+++ src/pages/Trecipe/DestinationCard/DestinationCard.tsx
@@ -3,6 +3,7 @@ import { Image } from "../../../components/Image/Image";
 import "./DestinationCard.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Rating, RatingBar } from "../../../components/Rating/RatingBar";
+import { Draggable } from "react-beautiful-dnd";
 
 /**
  * Destination Category
@@ -37,27 +38,26 @@ export interface DestinationModel {
  * index: where in Destination card list this one is located (needed for Drag & Drop functionality)
  * destModal: destination model to render
  * onClickDelete: callback handler for delete button
+ * isInEdit: true if currently in edit, false otherwise
  */
 export interface DCProps {
   index: number;
   destModel: DestinationModel;
   onClickDelete: (e: React.MouseEvent) => void;
+  isInEdit: boolean;
 }
 
 /**
  * DCState
  * isCompleted: true if destination is checked off, false otherwise
- * isInEdit: true if currently in edit, false otherwise
  */
 export interface DCState {
   isCompleted: boolean;
-  isInEdit: boolean;
 }
 
 export class DestinationCard extends React.Component<DCProps, DCState> {
   state: Readonly<DCState> = {
     isCompleted: false,
-    isInEdit: false,
   };
 
   private handleCompletedCheck() {
@@ -68,45 +68,61 @@ export class DestinationCard extends React.Component<DCProps, DCState> {
 
   render() {
     return (
-      <div className="dest-card-wrapper">
-        <div className="dest-img">
-          <Image src={this.props.destModel.imgSrc} />
-        </div>
-        <div className="dest-info">
-          <h6 className="dest-category">{this.props.destModel.category}</h6>
-          <h6 className="dest-address">{this.props.destModel.address}</h6>
-          <h3 className="dest-name">{this.props.destModel.name}</h3>
-          <RatingBar rating={this.props.destModel.rating} />
-          <p className="dest-description">{this.props.destModel.description}</p>
-        </div>
-        {!this.state.isInEdit && (
-          <div className="completed-checkbox">
-            <input
-              type="checkbox"
-              id={this.props.destModel.id + "-completed"}
-              onChange={this.handleCompletedCheck.bind(this)}
-            />
-            <label
-              htmlFor={this.props.destModel.id + "-completed"}
-              className="check-mark">
-              <FontAwesomeIcon icon="check" />
-            </label>
+      <Draggable
+        draggableId={String(this.props.destModel.id)}
+        index={this.props.index}
+        isDragDisabled={!this.props.isInEdit}>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.draggableProps}>
+            <div className="dest-card-wrapper">
+              <div className="dest-img">
+                <Image src={this.props.destModel.imgSrc} />
+              </div>
+              <div className="dest-info">
+                <h6 className="dest-category">
+                  {this.props.destModel.category}
+                </h6>
+                <h6 className="dest-address">{this.props.destModel.address}</h6>
+                <h3 className="dest-name">{this.props.destModel.name}</h3>
+                <RatingBar rating={this.props.destModel.rating} />
+                <p className="dest-description">
+                  {this.props.destModel.description}
+                </p>
+              </div>
+              {!this.props.isInEdit && (
+                <div className="completed-checkbox">
+                  <input
+                    type="checkbox"
+                    id={this.props.destModel.id + "-completed"}
+                    onChange={this.handleCompletedCheck.bind(this)}
+                  />
+                  <label
+                    htmlFor={this.props.destModel.id + "-completed"}
+                    className="check-mark">
+                    <FontAwesomeIcon icon="check" />
+                  </label>
+                </div>
+              )}
+              {this.props.isInEdit && (
+                <div className="edit-options-wrapper">
+                  <span
+                    {...provided.dragHandleProps}
+                    className="edit-option"
+                    id="dest-reorder">
+                    <FontAwesomeIcon icon="bars" />
+                  </span>
+                  <button
+                    className="edit-option"
+                    id="dest-delete"
+                    onClick={(e) => this.props.onClickDelete(e)}>
+                    <FontAwesomeIcon icon={["far", "trash-alt"]} />
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         )}
-        {this.state.isInEdit && (
-          <div className="edit-options-wrapper">
-            <span className="edit-option" id="dest-reorder">
-              <FontAwesomeIcon icon="bars" />
-            </span>
-            <button
-              className="edit-option"
-              id="dest-delete"
-              onClick={this.props.onClickDelete}>
-              <FontAwesomeIcon icon={["far", "trash-alt"]} />
-            </button>
-          </div>
-        )}
-      </div>
+      </Draggable>
     );
   }
 }
