8f7cb2f8e682bf1519de5b4974bcf32290578e76
knox153 <36020702+knox153@users.noreply.github.com>
2020-07-22 18:52:42 -0700

    Setup passport & user routes
    - Passport is added as a middleware
    - Setup passport to use cookie and jwt token for authentication
    - Added user routes required for authentication
    - Updated api documentation which  will be used for validation

--- packages/server/server/routes.ts
+++ packages/server/server/routes.ts
@@ -2,8 +2,14 @@ import { Application } from 'express';
 import TrecipeController from './api/trecipe/trecipe.controller';
 import Controller from './common/controller';
 import DestinationController from './api/destinations/destination.controller';
+import UserController from './api/user/user.controller';
+
 export default function routes(app: Application): void {
-    const controllers = [new TrecipeController(), new DestinationController()];
+    const controllers = [
+        new TrecipeController(),
+        new DestinationController(),
+        new UserController(),
+    ];
     controllers.forEach((controller: Controller) => {
         app.use('/api/v1', controller.router);
     });
