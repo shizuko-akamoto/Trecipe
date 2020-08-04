08b4fcb8a15f9eb0259814c16ef23a8c2248b77f
tianyan-zhu <tianyan.zhu@alumni.ubc.ca>
2020-08-03 19:10:06 -0700

    initial landing page completed; added carousel
    finish dynamic carousel
    
    change placeholder for intro-text
    
    added svg to the banner
    
    remove outline when image is selected
    
    Squash some commits.

--- packages/client/src/pages/Landing/Landing.tsx
+++ packages/client/src/pages/Landing/Landing.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import "./Landing.scss";
 class Landing extends React.Component {
     render() {
-        return <div>Landing page placeholder</div>;
+        return (
+            <div className='container'>
+                <div className='carousel-container'>
+                    <Carousel autoPlay={false} showThumbs={false} useKeyboardArrows={true} infiniteLoop={true} showStatus={false} stopOnHover={true} interval={5000}>
+                        <div className='intro-banner'>
+                            <img className='intro-image'/>
+                            <h1 className="intro-text">Trecipe</h1>
+                            <svg
+                                className="border"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 100 100"
+                                preserveAspectRatio="none">
+                                <path d="M 0 0 Q 50 50 100 0 V 100 H 0 Z" />
+                            </svg>
+                        </div>
+                        <div className='intro-banner'> 
+                            <img className='intro-image'/>
+                            <h1 className="intro-text">Trecipe</h1>
+                            <svg
+                                className="border"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 100 100"
+                                preserveAspectRatio="none">
+                                <path d="M 0 0 Q 50 50 100 0 V 100 H 0 Z" />
+                            </svg>
+                        </div>
+                        <div className='intro-banner'>
+                            <img className='intro-image'/>
+                            <h1 className="intro-text">Trecipe</h1>
+                            <svg
+                                className="border"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 100 100"
+                                preserveAspectRatio="none">
+                                <path d="M 0 0 Q 50 50 100 0 V 100 H 0 Z" />
+                            </svg>
+                        </div>
+                    </Carousel>
+                </div>
+                <div className='content-container'>
+                    <div className='content-wrapper'>
+                        Content Placeholder
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
