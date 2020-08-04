2ecda0e3603725f902df179187e5b48d7e4ed1bb
shizuko <aka-shizuko@hotmail.com>
2020-08-04 05:12:46 -0400

    Update build script to copy rsa public key into build folder

--- packages/server/build.ts
+++ packages/server/build.ts
@@ -5,5 +5,6 @@ const outDir = config.compilerOptions.outDir;
 s.rm('-rf', outDir);
 s.mkdir(outDir);
 s.cp('.env', `${outDir}/.env`);
-s.mkdir('-p', `${outDir}/server/server/common/swagger`);
+s.mkdir('-p', `${outDir}/server/server/common/passport`);
 s.cp('server/common/api.yml', `${outDir}/server/server/common/api.yml`);
+s.cp('server/common/passport/id_rsa_pub.pem', `${outDir}/server/server/common/passport/id_rsa_pub.pem`);
